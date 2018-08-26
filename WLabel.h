1c64cd9d739f5a5a3df186a960fb11bfbb333f9e
shichonghuotian <565672281@qq.com>
2018-08-26 15:08:15 +0800

    t

---
+++ WLabel.h
@@ -0,0 +1,15 @@
+//
+//  WButton.h
+//  WButtonKit_Example
+//
+//  Created by Apple on 2018/8/26.
+//  Copyright © 2018年 shichonghuotian@163.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WLabel : NSObject
+
+
+
+@end
---
+++ WLabel.m
@@ -0,0 +1,23 @@
+//
+//  WButton.m
+//  WButtonKit_Example
+//
+//  Created by Apple on 2018/8/26.
+//  Copyright © 2018年 shichonghuotian@163.com. All rights reserved.
+//
+
+#import "WLabel.h"
+
+@implementation WLabel
+-(instancetype)init {
+    
+    self = [super init];
+    
+    if(self) {
+        
+        
+    }
+    
+    return self;
+}
+@end
